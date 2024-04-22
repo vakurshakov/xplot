25702b09fc63d326fb53421d59a79e8fccfd3194
Vladislav Kurshakov <vakurshakov@gmail.com>
2024-04-22 14:22:01 +0700

    linear stage of m=3 growth annotation added

--- final_beta_center.py
+++ final_beta_center.py
@@ -20,7 +20,7 @@ plot.set_axes_args(
     xlim=(0, 9),
     xticks=np.linspace(0, 9, 7),
 
-    ylim=(0, 1.2),
+    ylim=(0, 1.3),
     # yticks=np.linspace(0, 9, 5),
 )
 
@@ -31,14 +31,34 @@ pd = np.load(f"{params_path}/Data1D/pd_t.npy")
 pb = 1 - np.square(b)
 
 ts = np.arange(0, int(9 * tau / dts) + 1) * dts / tau
-_ts = sliding_average(ts)
+
+w = 10
+_ts = sliding_average(ts, w)
 
 ax = plot.axes_position
-ax.plot(_ts, sliding_average(pr), label="$\\beta_r$")
-ax.plot(_ts, sliding_average(pr - pd), label="$\\beta_r - \\Delta \\beta$")
+ax.plot(_ts, sliding_average(pr, w), label="$\\beta_r$")
+ax.plot(_ts, sliding_average(pr - pd, w), label="$\\beta_r - \\Delta \\beta$")
 ax.plot(ts, pb, label="$\\beta_b$")
 
-ax.legend(fontsize=ssmol)
+tmin = int(3.5 * tau / dts)
+tmax = int(6.5 * tau / dts)
+ax.fill_between(
+    sliding_average(ts[tmin:tmax], w),
+    sliding_average(pr[tmin:tmax], w),
+    np.zeros_like(sliding_average(pr[tmin:tmax], w)),
+    color="grey", 
+    alpha=0.2,
+    zorder=-1
+)
+
+ax.annotate(
+    "${\\rm linear~stage~of}~m=3~{\\rm growth}$",
+    (4.8, 0.9),
+    (3.1, 0.5),
+    **arg_anno,
+)
+
+ax.legend(fontsize=ssmol, framealpha=1)
 ax.grid(alpha=0.3)
 plot.draw_info()
 
