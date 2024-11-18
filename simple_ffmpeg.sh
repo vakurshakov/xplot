648fdc1e55ac105bdc80c9b402b99e4092d286d9
vakurshakov <crimsonschist@gmail.com>
2024-11-18 14:50:36 +0700

    optional renaming

--- simple_ffmpeg.sh
+++ simple_ffmpeg.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 if [[ $# == 0 ]]; then
-    echo "Directory is needed, dumbass!"
+    echo "Directory is needed"
     exit
 fi
 
 params_path=$1
-shift 1 
+shift 1
+
+rename=${1:-0}
+shift 1
 
 if [[ $# == 0 ]]; then
     diagnostics=(
@@ -22,12 +25,13 @@ fi
 for d in ${diagnostics[*]}; do
     pushd ./$params_path/$d
 
-    # Renaming, if needed
-    i=0
-    for f in *; do
-        mv $f $(printf %04d $i)".png"
-        i=$((i + 1))
-    done
+    if [[ rename != 0 ]]; then
+        i=0
+        for f in *; do
+            mv $f $(printf %04d $i)".png"
+            i=$((i + 1))
+        done
+    fi
 
     ffmpeg -y -i ./%04d.png -r 15 ../Video/$d.mp4
 
