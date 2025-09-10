ff843ff69d573bec60acd24ab24c51da90c1b8ec
vakurshakov <crimsonschist@gmail.com>
2025-09-10 13:09:36 +0700

    os4 corrections: vacuum magnetic field is used for beta, ion and electron pressure components are summed

--- final/os4.py
+++ final/os4.py
@@ -16,7 +16,7 @@ ys = (np.arange(0, y_max) - y_max // 2) * dy
 
 nmap = (0, 4)
 bzmap = (0, 0.2)
-bpmap = (0, 1.5)
+bpmap = (0, 1)
 
 bz = [
     magnetic_field("Z", subplot(fig, gs, 0, 0), "", bzmap, unsigned_cmap),
@@ -24,14 +24,10 @@ bz = [
     magnetic_field("Z", subplot(fig, gs, 2, 0), "", bzmap, unsigned_cmap),
 ]
 
-s = "Ions"
-ms = (mi_me if s == "Ions" else 1)
-es = ((+1)  if s == "Ions" else (-1))
-
 ns = [
-    particles_field(s, "Density", "Z", subplot(fig, gs, 0, 1), f"$n_{s[0].lower()}$", nmap, unsigned_cmap),
-    particles_field(s, "Density", "Z", subplot(fig, gs, 1, 1), f"$n_{s[0].lower()}$", nmap, unsigned_cmap),
-    particles_field(s, "Density", "Z", subplot(fig, gs, 2, 1), f"$n_{s[0].lower()}$", nmap, unsigned_cmap),
+    particles_field("Ions", "Density", "Z", subplot(fig, gs, 0, 1), "$n_i$", nmap, unsigned_cmap),
+    particles_field("Ions", "Density", "Z", subplot(fig, gs, 1, 1), "$n_i$", nmap, unsigned_cmap),
+    particles_field("Ions", "Density", "Z", subplot(fig, gs, 2, 1), "$n_i$", nmap, unsigned_cmap),
 ]
 
 bzl = magnetic_field(         "Z", subplot(fig, gs, 3, 0), "")
@@ -53,34 +49,59 @@ def update_args(d, title, map):
 update_args(bzl, "Magnetic field, $B_z(y)$", bzmap)
 update_args(bpl, "Plasma beta, $\\beta(y)$", bpmap)
 
-pr = particles_field(s, "Prr", "Z")
-pa = particles_field(s, "Ppp", "Z")
-pr.data = [None]*len(bz)
-pa.data = [None]*len(bz)
+pr_i = particles_field("Ions", "Prr", "Z")
+pa_i = particles_field("Ions", "Ppp", "Z")
+pr_e = particles_field("Electrons", "Prr", "Z")
+pa_e = particles_field("Electrons", "Ppp", "Z")
+pr_i.data = [None]*len(bz)
+pa_i.data = [None]*len(bz)
+pr_e.data = [None]*len(bz)
+pa_e.data = [None]*len(bz)
 
 TAU = [4, 7, int(time / tau)]
 
 def line_average(d):
     return np.mean(d[:,xc-xw:xc+xw], axis=1)
 
+bz0 = line_average(get_parsed_field(bz[0], "B", "Z", "z", 0))
+nmax = [None]*len(bz)
+
+phi = np.linspace(0, 2 * np.pi)
+xr = rr * np.cos(phi)
+yr = rr * np.sin(phi)
+
 for i, tt in enumerate(TAU):
     t = int(tt * tau / dts)
 
     bz[i].axes_args["title"] = f"$B_z(x,\\,y,\\,t/\\tau = {tt})$"
-    ns[i].axes_args["title"] = f"$n_{s[0].lower()}(x,\\,y,\\,t/\\tau = {tt})$"
+    ns[i].axes_args["title"] = f"$n_i(x,\\,y,\\,t/\\tau = {tt})$"
 
     bz[i].data = get_parsed_field(bz[i], "B", "Z", "z", t) 
     ns[i].data = get_parsed_scalar(ns[i], t) 
-    pr.data[i] = line_average(get_parsed_scalar(pr, t))
-    pa.data[i] = line_average(get_parsed_scalar(pa, t))
+    nmax[i] = np.argmax(ns[i].data)
+
+    pd =  line_average(get_parsed_scalar(pr_i, t)) + line_average(get_parsed_scalar(pa_i, t))
+    pd += line_average(get_parsed_scalar(pr_e, t)) + line_average(get_parsed_scalar(pa_e, t))
+    pd /= 2
 
     bzl.data[i] = line_average(bz[i].data)
-    bpl.data[i] = (pr.data[i] + pa.data[i]) / np.square(bzl.data[i])
+    bpl.data[i] = pd / (np.square(bz0) / 2)
 
-for d in [ *bz, *ns ]:
-    d.draw(add_cbar=True)
-    d.draw_info()
-    d.axes_position.set_aspect(1)
+    def draw(d):
+        d.draw(add_cbar=True)
+        d.draw_info()
+        ax = d.axes_position
+        ax.set_aspect(1)
+        ax.plot(xr, yr, linewidth=3, linestyle="--", c="black")
+        
+        y0, x0 = np.unravel_index(nmax[i], ns[i].data.shape)
+        x0 = (x0 - xc) * dx
+        y0 = (y0 - xc) * dx
+        ax.scatter(x0, y0, c="black", s=150*1.6, zorder=10)
+        ax.scatter(x0, y0, c="red",   s=150*1.0, zorder=10)
+
+    draw(bz[i])
+    draw(ns[i])
 
 for i, tt in enumerate(TAU):
     bzl.axes_position.plot(ys, bzl.data[i], label=f"$t/\\tau = {tt}$", linewidth=3)
