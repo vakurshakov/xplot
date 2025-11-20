8f2da8182a1b9b6505a74c4c9c874f13dfcaf895
vakurshakov <crimsonschist@gmail.com>
2025-11-20 14:11:02 +0700

    adding colorbars normally, renamging instability increment

--- final/os8.py
+++ final/os8.py
@@ -19,18 +19,12 @@ import ConductiveWall_FixRotor.parameters_circle as ParamsCircle
 import ConductiveWall_FixRotor.parameters_square as ParamsSquare
 
 ncols=4
-nrows=4
+nrows=5
 
 fig = plt.figure(figsize=(8, 10))
-gs = GridSpec(ncols=ncols, nrows=nrows, width_ratios=[1] * ncols, height_ratios=[1, 1, 1, 1.7], figure=fig)
+gs = GridSpec(ncols=ncols, nrows=nrows, width_ratios=[1] * ncols, height_ratios=[1, 1, 1, 0.06, 1.7], figure=fig)
 
-y_max = data_shape["Z"][1]
-xc    = data_shape["Z"][0] // 2
-xw    = 2
-
-ys = (np.arange(0, y_max) - y_max // 2) * dy
-
-nmap = (0, 4)
+nmap = (0, 5)
 bzmap = (0, 0.2)
 bpmap = (0, 1)
 
@@ -54,6 +48,8 @@ ns = [
     particles_field("Ions", "Density", "Z", subplot(fig, gs, 3, 2), "", nmap, unsigned_cmap), # t = 10, Square
 ]
 
+time = min(ParamsCircle.time, ParamsSquare.time)
+
 TAU = [4, 7, int(time / tau)]
 
 nmax = [None]*len(bz)
@@ -62,8 +58,28 @@ phi = np.linspace(0, 2 * np.pi)
 xr = rr * np.cos(phi)
 yr = rr * np.sin(phi)
 
-def draw(d, j):
+def cbar(d, n, cax):
+    fig = d.axes_position.figure
+    cax.tick_params(labelsize=Fonts.ssmol, pad=6)
+    fig.colorbar(
+        d.im,
+        orientation="horizontal",
+        cax=cax,
+        ticks=np.linspace(d.vmin_vmax[0], d.vmin_vmax[1], n),
+    )
+
+def draw(d, i, j):
     d.draw()
+
+    if d == bz[2]:
+        cbar(d, 5, subplot(fig, gs, 0, 3))
+    if d == bz[5]:
+        cbar(d, 5, subplot(fig, gs, 2, 3))
+    if d == ns[2]:
+        cbar(d, 6, subplot(fig, gs, 1, 3))
+    if d == ns[5]:
+        cbar(d, 6, subplot(fig, gs, 3, 3))
+        
     d.draw_info()
     ax = d.axes_position
     ax.set_aspect(1)
@@ -71,10 +87,14 @@ def draw(d, j):
     
     nmax[j] = np.argmax(ns[j].data)
     y0, x0 = np.unravel_index(nmax[j], ns[j].data.shape)
+    xc = ns[j].data.shape[0] / 2
     x0 = (x0 - xc) * dx
     y0 = (y0 - xc) * dx
     ax.scatter(x0, y0, c="black", s=6*1.6, zorder=10)
     ax.scatter(x0, y0, c="red",   s=6*1.0, zorder=10)
+
+bbox2 = dict(bbox)
+bbox2["linewidth"] = 0.5
 
 for i, tt in enumerate(TAU):
     t = int(tt * tau / dts)
@@ -98,7 +118,7 @@ for i, tt in enumerate(TAU):
 
 
         if j == i:
-            fig.text(0.06, 0.970 - 0.21 * i, f"$t/\\tau = {tt}$", size=Fonts.big, ha="center", bbox=bbox)
+            fig.text(0.043, 0.9605 - 0.1937 * i, f"$t/\\tau = {tt}$", size=Fonts.big, ha="center", bbox=bbox2)
             prefix = get_prefix(t, ParamsCircle.restart_timesteps, ParamsCircle.prefixes)        
         else:
             prefix = get_prefix(t, ParamsSquare.restart_timesteps, ParamsSquare.prefixes)        
@@ -106,12 +126,12 @@ for i, tt in enumerate(TAU):
         bz[j].data = get_parsed_field(bz[j], "B", "Z", "z", t, prefix) 
         ns[j].data = get_parsed_scalar(ns[j], t, prefix) 
 
-        draw(bz[j], j)
-        draw(ns[j], j)
+        draw(bz[j], i, j)
+        draw(ns[j], i, j)
 
 ############# FOURIER ############
 
-TIME = d_tmax
+TIME = int(10 * tau / dts)
 TS = np.arange(0, TIME) * dts / tau
 R = 10.0
 MAX_M = 5
@@ -122,7 +142,7 @@ def prepare_field_m(ax, label, data):
     F_tm.set_axes_args(
         xlabel="$t / \\tau$",
         xlim=(0, TIME * dts / tau),
-        xticks=np.linspace(0, TIME * dts / tau, 7),
+        xticks=np.linspace(0, TIME * dts / tau, 6),
     )
 
     ## Merge it back into `fourier_transform()` ##
@@ -152,7 +172,7 @@ compare_models = [
 ]
 
 for j, (label, path) in enumerate(compare_models):
-    ax = fig.add_subplot(gs[3, j*2:j*2+2])
+    ax = fig.add_subplot(gs[4, j*2:j*2+2])
 
     F_at = prepare_field_phit(None, "ni", "", R, 1.0, path)
     F_mw, m = prepare_field_m(ax, label, F_at.data)
@@ -170,7 +190,7 @@ for j, (label, path) in enumerate(compare_models):
     Gr = (Ga / tau) * (L / V)
 
     if j == 0:
-        ax.plot(tt, 0.38 * np.exp(Ga * tt), linestyle="--", color="C0", linewidth=1.5, label=f"$\\Gamma \\approx {Gr:.2f} \\, v_{{Ti}} / L$")
+        ax.plot(tt, 0.38 * np.exp(Ga * tt), linestyle="--", color="C0", linewidth=1.5, label=f"$\\Gamma \\approx {Gr:.2f} \\, \\Gamma_{{fl}}$")
         # ax.set_title(label, pad=0, y=1.05)
         ax.legend(loc="upper left", fontsize=8.5, framealpha=1.0) #bbox_to_anchor=(0.44, 0.55),
     elif j == 1:
@@ -179,7 +199,7 @@ for j, (label, path) in enumerate(compare_models):
     ax.set_ylim(1e-2, 1e+3)
     ax.grid(alpha=0.6)
 
-fig.text(0.44, 0.30, f"$\\delta n_i(t, m, r = {R:.1f})$", size=Fonts.big, ha="center", bbox=bbox)
+fig.text(0.415, 0.290, f"$\\delta n_i(t, m, r = {R:.1f})$", size=Fonts.big, ha="center", bbox=bbox2)
 
-fig.tight_layout(h_pad=0.1, w_pad=0.1, rect=(0, 0, 1, 0.99))
+fig.tight_layout(h_pad=0.1, w_pad=0.1, rect=(0, 0, 1, 0.98))
 fig.savefig(f"{res_dir}/os8.pdf")
